07a64937bad47b8189579238118eff57a4d27212
Shaun Read <gboff21@gmail.com>
2015-04-22 19:55:10 +0100

    initial commit: template code

---
+++ multithreader.py
@@ -0,0 +1,59 @@
+import Queue  # or queue in Python 3
+import threading
+
+class PrintThread(threading.Thread):
+	def __init__(self, queue):
+		threading.Thread.__init__(self)
+		self.queue = queue
+
+	def printfiles(self, p):
+		for path, dirs, files in os.walk(p):
+			for f in files:
+				print(f, file=output)
+
+	def run(self):
+		while True:
+			result = self.queue.get()
+			self.printfiles(result)
+			self.queue.task_done()
+
+class ProcessThread(threading.Thread):
+	def __init__(self, in_queue, out_queue):
+		threading.Thread.__init__(self)
+		self.in_queue = in_queue
+		self.out_queue = out_queue
+
+	def run(self):
+		while True:
+			path = self.in_queue.get()
+			result = self.process(path)
+			self.out_queue.put(result)
+			self.in_queue.task_done()
+
+	def process(self, path):
+		# Do the processing job here
+if __name__ == '__main__':
+	pathqueue = Queue.Queue()
+	resultqueue = Queue.Queue()
+	paths = getThisFromSomeWhere()
+
+	output = codecs.open('file', 'a')
+
+	# spawn threads to process
+	for i in range(0, 5):
+		t = ProcessThread(pathqueue, resultqueue)
+		t.setDaemon(True)
+		t.start()
+
+	# spawn threads to print
+	t = PrintThread(resultqueue)
+	t.setDaemon(True)
+	t.start()
+
+	# add paths to queue
+	for path in paths:
+		pathqueue.put(path)
+
+	# wait for queue to get empty
+	pathqueue.join()
+	resultqueue.join()
