4ab2c2a53975ea6ce09546fda23efa796a55d486
Shaun Read <gboff21@gmail.com>
2015-04-22 20:26:18 +0100

    reformatted for purpose

--- multithreader.py
+++ multithreader.py
@@ -1,59 +1,77 @@
 import Queue  # or queue in Python 3
 import threading
+import logging
 
 class PrintThread(threading.Thread):
 	def __init__(self, queue):
-		threading.Thread.__init__(self)
+		super(PrintThread, self).__init__()
 		self.queue = queue
-
-	def printfiles(self, p):
-		for path, dirs, files in os.walk(p):
-			for f in files:
-				print(f, file=output)
+	
+	def write(self, p):
+		print p
 
 	def run(self):
 		while True:
 			result = self.queue.get()
-			self.printfiles(result)
+			self.write(result)
 			self.queue.task_done()
 
+class WriteThread(PrintThread):
+	def __init__(self, queue, fname):
+		super(WriteThread, self).__init__(queue)
+		self.fname = fname
+
+	# def write(self, p):
+	# 	with open(self.fname, 'a') as f:
+	# 		f.write(u'{}\n'.format(p).encode('utf-8'))
+
 class ProcessThread(threading.Thread):
-	def __init__(self, in_queue, out_queue):
-		threading.Thread.__init__(self)
+	def __init__(self, in_queue, out_queue, process_function=lambda task: 1/0):
+		super(ProcessThread, self).__init__()
 		self.in_queue = in_queue
 		self.out_queue = out_queue
+		self.process = process_function
 
 	def run(self):
 		while True:
 			path = self.in_queue.get()
-			result = self.process(path)
-			self.out_queue.put(result)
-			self.in_queue.task_done()
+			try:
+				result = self.process(path)
+				self.out_queue.put(result)
+				self.in_queue.task_done()
+			except:
+				logging.critical('Process {}'.format(self.name), exc_info=True)
+				self.in_queue.task_done()
 
-	def process(self, path):
-		# Do the processing job here
+
 if __name__ == '__main__':
-	pathqueue = Queue.Queue()
-	resultqueue = Queue.Queue()
-	paths = getThisFromSomeWhere()
+	logging.basicConfig(filename='multithreader.log', format='[%(asctime)s] %(message)s', filemode='w')
 
-	output = codecs.open('file', 'a')
+	TaskQueue = Queue.Queue()
+	ResultQueue = Queue.Queue()
+	# PrintQueue = Queue.Queue()
+	
+	tasks = xrange(10)
+
+	out_dir = 'file.txt'
 
 	# spawn threads to process
+	workers = []
 	for i in range(0, 5):
-		t = ProcessThread(pathqueue, resultqueue)
+		t = ProcessThread(TaskQueue, ResultQueue)
 		t.setDaemon(True)
+		workers.append(t)
 		t.start()
 
 	# spawn threads to print
-	t = PrintThread(resultqueue)
-	t.setDaemon(True)
-	t.start()
+	writer = WriteThread(ResultQueue, out_dir)
+	writer.setDaemon(True)
+	writer.start()
 
 	# add paths to queue
-	for path in paths:
-		pathqueue.put(path)
+	for task in tasks:
+		TaskQueue.put(task)
 
-	# wait for queue to get empty
-	pathqueue.join()
-	resultqueue.join()
+	# wait for queues to get empty
+	TaskQueue.join()
+	ResultQueue.join()
